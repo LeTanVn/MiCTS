1e3b56b764a20cfc057bf6405af1e294f188a91f
LeTanVn <92218083+LeTanVn@users.noreply.github.com>
2026-05-25 21:36:49 +0700

    Launch MainActivity directly with isolated taskAffinity to avoid clearing task and kicking to homescreen

--- app/src/main/java/com/parallelc/micts/ui/OverlayService.kt
+++ app/src/main/java/com/parallelc/micts/ui/OverlayService.kt
@@ -147,7 +147,7 @@ class OverlayService : Service() {
 
     private fun triggerCTS() {
         val intent = Intent(this, com.parallelc.micts.ui.activity.MainActivity::class.java)
-        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NO_ANIMATION)
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_NO_ANIMATION)
         startActivity(intent)
     }
 
